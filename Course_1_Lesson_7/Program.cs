f879dac2b2dc92b732a918156d339c4399d984ef
Artem <Artemrid@yandex.ru>
2022-06-04 16:02:32 +0300

    Homework #7

---
+++ Course_1_Lesson_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_1_Lesson_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Сегодня жарко?");
+            string str = Console.ReadLine();
+            if (str == "Да")
+            {
+                Console.WriteLine("Мне тоже!");
+            }
+            else
+            {
+                Console.WriteLine("Ну и ладно...");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
